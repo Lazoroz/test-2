02d65f8532e75665c483ccc384d983d17d994e0b
Lazoroz <ramlal524@gmail.com>
2023-05-23 17:06:35 +0200

    role middleware added

--- database/migrations/2023_05_22_121632_create_mentors_table.php
+++ database/migrations/2023_05_22_121632_create_mentors_table.php
@@ -16,9 +16,6 @@ return new class extends Migration
             $table->string('voornaam');
             $table->string('achternaam');
             $table->string('klas');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
